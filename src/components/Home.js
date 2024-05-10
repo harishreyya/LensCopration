6319cff8387c20e868ff1ad45da7b81fb16caa12
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-05-11 01:05:14 +0530

    about

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
 export const Home = () =>{
 
     return <div>
-            <header>
-                
+            <header className="flex header">
+                <img className="" src="https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnav_logo.e5fb945a.png&w=256&q=75" alt="logo"/>
+
+                <div className="flex header-text">
+                    <p>MakeMyWeb</p>
+                    <p className="gradient-text">Home</p>
+                    <p>Company</p>
+                    <p>Blogs</p>
+                </div>
             </header>
+
+            <div className="first-section">
+                <div>  <h1>We are at the forefront of AI</h1>
+                <p>From Conserving Wildlife to Automatically Generating Caricatures<b>– We Do It All</b></p>
+            <button className="learn-more flex">Learn More</button>
+            </div>
+              
+            </div>
     </div>
 }
