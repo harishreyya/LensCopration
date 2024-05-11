48c763540a1de2fbd055700499de263deef9f0cf
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-05-11 14:09:40 +0530

    services

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,33 +8,33 @@ export const Services = () =>{
  <h1>We provide Artificial Intelligence Services</h1>
  <div className="service-grid-parent">
 <div className="service-grid">
-<div >
+<div className="biometric">
     <div className="flex">
-    <h2>01</h2>
+    <h2>0<span>1</span></h2>
     <h3>Biometrics</h3>
     </div>
 
     <p>Academia-backed & In-house researched State-of-the-Art Face, Fingerprint, and Iris Recognition SDKs. Enable real-time automated Biometric applications on edge devices even without an active internet connection.</p>
 </div>
-<div>
+<div className="image-analyis">
 <div className="flex">
-    <h2>02</h2>
+    <h2>0<span>2</span></h2>
     <h3>Image Analysis</h3>
     </div>
 
     <p>Outsource the overly complex image analysis work to our intelligent machines that adaptively learn, so you can focus on making the best decisions for your business.</p>
 </div>
-<div>
+<div className="cross-media">
 <div className="flex">
-    <h2>03</h2>
+    <h2>0<span>3</span></h2>
     <h3>Cross-Media Translation</h3>
     </div>
 
     <p>Will something like Siri or Alexa enhance your business? We can deliver text-to-speech, text-to-image, speech-to-text, speech-to-image, speech-to-image, image-to-text and image-to-speech solutions for maximum convenience.</p>
 </div>
-<div>
+<div className="modeling">
 <div className="flex">
-    <h2>04</h2>
+    <h2>0<span>4</span></h2>
     <h3>3D modeling and design.</h3>
     </div>
 
@@ -45,7 +45,7 @@ export const Services = () =>{
 
 </div>
 </div>
-
+<br/><br/><br/><br/>
 </div>
 
     </div>
