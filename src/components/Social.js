44410315ac584079c7b6bfe8a987f024ab785d65
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-05-13 12:51:02 +0530

    social

---
+++ src/components/Social.js
@@ -0,0 +1,13 @@
+export const Social = ()=>{
+    return <div>
+<div className="social-section">
+<br/>
+ <h2>OUR VISION</h2>
+    <br/>
+ <div className="underline-about"></div>
+ <h1>AI for Social Good</h1>
+ <p>Explainable AI (XAI) is an emerging subject of machine learning research that refers to strategies that try to provide transparency to typically opaque AI models and their predictions.</p>
+</div>
+
+    </div>
+}
--- src/components/Solutions.js
+++ src/components/Solutions.js
@@ -6,40 +6,54 @@ export const Solutions = ()=>{
     <br/>
  <div className="underline-about"></div>
  <h1>AI-driven solutions backed by science</h1>
-<p>Every piece of AI technology shipped from LENS is thoroughly benchmarked via rigorous evaluations. With a global network of experts and academicians, we guarantee the most accurate and robust solutions in the market.</p>
+<p className="ai-text">Every piece of AI technology shipped from LENS is thoroughly benchmarked via rigorous evaluations. With a global network of experts and academicians, we guarantee the most accurate and robust solutions in the market.</p>
 
 <div className="flex Tick-solutions">
 <div>
-<p>State-of-the-art solutions </p>
-<p>Fast & Efficient</p>
-<p>No extra computation fee</p>
-<p>No licensing fee</p>
+<p>&#10003; State-of-the-art solutions </p>
+<p>&#10003; Fast & Efficient</p>
+<p>&#10003; No extra computation fee</p>
+<p>&#10003; No licensing fee</p>
 </div>
 
 <div>
-<p>Lifetime support & upgrades</p>
-<p>Plug-and-Play</p>
-<p>24x7 Progress Monitoring</p>
-<p>incremental Updates</p>
+<p>&#10003; Lifetime support & upgrades</p>
+<p>&#10003; Plug-and-Play</p>
+<p>&#10003; 24x7 Progress Monitoring</p>
+<p>&#10003; incremental Updates</p>
 </div>
 
 </div>
 
-{/*  */}
+
 <div>
+<div className="flex ai-boxes">
 <div>
-<div></div>
-<div></div>
-<div></div>
+<h2>Exclusive Rights</h2>
+<img src="" alt=""/>
+<p>Our mission is to make an impact in empowering human society with AI. Hence, all Intellectual Property Rights belongs to you.</p>
 </div>
+
 <div>
-<div></div>
+<h2>Research Driven</h2>
+<img src="" alt=""/>
+<p>We regularly benchmark our solutions via comparing industry-vide evaluations so our partners only get the best that AI can offer.</p>
 </div>
 
+<div>
+<h2>Plug-and-Play</h2>
+<img src="" alt=""/>
+<p>We provide AI-driven solutions into businesses where they can bring tangible value. Each solution is customized as per your needs and deployed on any computing device of your choice.</p>
 </div>
-
-
-
+</div>
+<div className="flex ai-last-card">
+<div>
+<h2>Lifetime Support</h2>
+<img src="" alt=""/>
+<p>Should you face any issues, we are always at your service. We provide lifetime technical support & upgrade options.</p>
+</div>
+</div>
+</div>
 </div>
 
     </div>
